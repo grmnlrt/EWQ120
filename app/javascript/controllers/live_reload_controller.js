a9e73d65e11f1ffd3dd0e7ace0405f4dcb7a0ec2
Germain Loret <germain.loret@gmail.com>
2020-03-25 12:30:58 +0100

    implemted live reload

--- app/javascript/controllers/live_reload_controller.js
+++ app/javascript/controllers/live_reload_controller.js
@@ -1,5 +1,28 @@
 import { Controller } from 'stimulus';
 
 export default class extends Controller {
+  static targets = ['preview', 'input']
 
+  connect() {
+    this.displayHtml();
+  }
+
+  displayHtml() {
+    this.previewTarget.contentDocument.documentElement.innerHTML = this.updateHtml();
+  }
+
+  updateHtml() {
+    let html = this.rawHtml;
+    const variables = html.match(/(?<={{{)\w+(?=}}})/g);
+    variables.forEach((variable) => {
+      const input = this.inputTargets.find(input => input.name === variable);
+      if (input) html = html.replace(`{{{${variable}}}}`, input.value);
+    })
+    return html;
+  }
+
+  get rawHtml() {
+    // simulates fetch to sendgrid API
+    return this.data.get('rawHtml');
+  }
 }
