6fd1cbd48eaf58e745683f2db004a9bac925abaf
Germain Loret <germain.loret@gmail.com>
2020-03-25 18:37:35 +0100

    done

--- app/javascript/controllers/live_reload_controller.js
+++ app/javascript/controllers/live_reload_controller.js
@@ -1,5 +1,27 @@
 import { Controller } from 'stimulus';
 
 export default class extends Controller {
+  static targets = ['preview', 'input'];
 
+  connect() {
+    this.displayHTML();
+  }
+
+  displayHTML() {
+    this.previewTarget.contentDocument.documentElement.innerHTML = this.updateHtml();
+  }
+
+  updateHtml() {
+    let html = this.rawHtml;
+    const variables = html.match(/(?<={{{)\w+(?=}}})/g);
+    variables.forEach((variable) => {
+      const input = this.inputTargets.find(input => input.name === variable);
+      if (input) html = html.replace(`{{{${variable}}}}`, input.value);
+    });
+    return html;
+  }
+
+  get rawHtml() {
+    return this.data.get('rawHtml');
+  }
 }
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,3 +29,6 @@ import { definitionsFromContext } from "stimulus/webpack-helpers"
 const application = Application.start()
 const context = require.context("../controllers", true, /\.js$/)
 application.load(definitionsFromContext(context))
+
+require("trix")
+require("@rails/actiontext")
